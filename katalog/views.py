c67ffb50736b4556c6a9c74f6843e862ca661bba
Qistina Muharrifa <qistinamuharrifa@qistina.local>
2022-09-15 06:27:12 +0700

    selesai

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Qistina Muharrifa',
+        'npm' : '2106708210'
+    }
+    return render(request, "katalog.html", context)
